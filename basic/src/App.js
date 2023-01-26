a7db7c0824dec1747aa14a7daebed6dde48ad2a0
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-26 15:50:40 -0500

    useState shouldn't work

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 
 function App() {
 
-  let num = Math.floor(Math.random()*10)
+  const [num, setNum]  = useState(Math.floor(Math.random()*10))
 
   return (
     <div className="App">
       <div>Your random number is:</div>
       <div>{num}</div>
     </div>
-    <div></div>
   );
 }
 
